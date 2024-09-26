10deae2d7f73de1e2dfb674f7a4f19686bef3a64
GCM6 <mingicelucky@gmail.com>
2024-09-26 18:20:52 +0800

    feat: add mdx page

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from 'mdx/types'
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
+  console.log('components', components)
   return {
     ...components,
   }
